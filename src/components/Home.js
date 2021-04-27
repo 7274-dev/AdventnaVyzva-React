9ba6004013686877cc9bbc4d2485878b2fab0c50
Daniel Svitan <dankosaurus9@gmail.com>
2021-04-27 06:56:56 +0200

    :zap: starts working on navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import { TeacherPage } from "./TeacherPage";
 import { StudentsPage } from "./StudentsPage";
 import { UniversalPage } from "./UniversalPage";
+import { Navbar } from "./Navbar";
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <UniversalPage />
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
-import "../styles/navbar.scss";
+import "../styles/navbar.css";
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="navbar">
+            <div className="navbar-link-container">
+                <a className="link-primary" href="/">Home</a>
+            </div>
         </div>
     )
 }
