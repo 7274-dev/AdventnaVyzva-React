b51bddaf3dbecf41cfbe7f69acd5da24195be521
Daniel Svitan <dankosaurus9@gmail.com>
2021-05-01 13:11:10 +0200

    :bulb: adds todos

--- src/components/StudentsPage.js
+++ src/components/StudentsPage.js
@@ -1,4 +1,7 @@
 const StudentsPage = () => {
+    // TODO: finish this page
+    // Scratch: https://cdn.discordapp.com/attachments/833685192249442315/836575903403475004/IMG_20210427_120218.jpg
+
     return (
         <div>
 
--- src/components/TeacherPage.js
+++ src/components/TeacherPage.js
@@ -1,6 +1,9 @@
 import "../styles/TeacherPage.css";
 
 const TeacherPage = () => {
+    // TODO: finish this page
+    // Scratch: https://cdn.discordapp.com/attachments/833685192249442315/836575903173443604/IMG_20210427_120223.jpg
+
     return (
         <div>
             <div className="navbar">
