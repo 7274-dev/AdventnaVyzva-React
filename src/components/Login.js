79afc126218283b5ee705f78c654e2ff93dd7581
Daniel Svitan <dankosaurus9@gmail.com>
2021-05-01 11:00:05 +0200

    :hammer: refactors Login.js code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,39 @@
-import { useRef } from "react";
+import { useState } from "react";
 import cookie from "react-cookies";
 import "../styles/Login.css";
 
 const Login = () => {
     const backendUrl = "http://localhost:7274";
-    const username = useRef();
-    const password = useRef();
-    const messageText = useRef();
+    const [usernameInput, setUsernameInput] = useState("");
+    const [passwordInput, setPasswordInput] = useState("");
+    const [messageText, setMessageText] = useState("");
 
-    const login = () => {
+    const login = async () => {
         // TODO: test if this works
 
-        const xml = new XMLHttpRequest();
-        xml.open("POST", backendUrl + "/login");
-        xml.setRequestHeader("Content-Type", "application/json");
+        let response = await fetch(backendUrl + "/login?username=" + usernameInput.value + ",password="
+            + passwordInput.value);
 
-        const data = {
-            username: username.current.value,
-            password: password.current.value
-        };
-
-        xml.onreadystatechange = () => {
-            if (xml.readyState === XMLHttpRequest.DONE) {
-                if (xml.status === 202) {
-                    cookie.save("token", JSON.stringify(xml.response).token);
-                    // cookie.load("token", false);
-                }
-                else if (xml.status === 418) {
-                    messageText.current.innerText = "Wrong password.";
-                }
-            }
+        if (response.ok) {
+            let json = await response.json();
+            cookie.save("token", json.token, []);
+            window.location = "/";
         }
-
-        xml.send(JSON.stringify(data));
+        else {
+            messageText.innerHTML = "Wrong username or password.";
+        }
     }
 
     return (
         <div className="login-container">
             <div className="login">
-                {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
-                <h1 ref={ node => messageText.current = node } />
+                <h1 ref={ node => setMessageText(node) } />
 
                 <h4>Username:</h4>
-                <input placeholder="Jozko Mrkvicka" ref={ node => username.current = node } />
+                <input placeholder="Jozko Mrkvicka" ref={ node => setUsernameInput(node) } />
 
                 <h4>Password:</h4>
-                <input placeholder="Heslo123" ref={ node => password.current = node } />
+                <input placeholder="Heslo123" ref={ node => setPasswordInput(node) } />
 
                 <button onClick={ login }>
                     <p>Log In</p>
