77876d6813594b2abd11900cf9a00dadbf7c5d9c
Ivan Hrabcak <ivan@hrabcak.eu>
2021-05-04 17:11:46 +0200

    :sparkles: Makes the home page look much better

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import { TeacherPage } from './TeacherPage';
 import { StudentsPage } from './StudentsPage';
 import { DelayedRedirect } from './DelayedRedirect';
+import { ReactComponent as TreeIcon } from '../images/tree.svg'
+import '../styles/Home.css';
 
 const Home = ({ isLoggedIn }) => {
     if (!isLoggedIn) {
         return (
             <>
+                <TreeIcon />
                 <h1>You have to log in to use this website!</h1><br/>
                 <h1>Redirecting...</h1>
                 <DelayedRedirect to="/login" delay={3500}/>
