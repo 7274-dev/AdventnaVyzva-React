905a112c539241b3abdc2a51f71dc8ce1519d19a
Daniel Svitan <dankosaurus9@gmail.com>
2021-05-22 07:56:24 +0200

    :zap: Adds try-catch to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,28 +8,47 @@ const Login = ({ setToken }) => {
     const [message, setMessage] = useState('');
 
     const login = async () => {
-        console.log({ usernameInput: username, passwordInput: password })
-        const result = await Api.login(username, password)
-
-        if (!result) {
-            setMessage('Wrong username or password.');
+        console.log({ usernameInput: username, passwordInput: password });
+        if (!username && password) {
+            setMessage("Username can't be empty!");
+            return;
         }
-        else {
-            setMessage(`You've successfully logged in!`);
-            setToken(result);
+        else if (username && !password) {
+            setMessage("Password can't be empty!");
+            return;
+        }
+        else if (!username && !password) {
+            setMessage("Password and username can't be empty!");
+            return;
+        }
+
+        try {
+            const result = await Api.login(username, password);
+
+            if (!result) {
+                setMessage('Wrong username or password.');
+            }
+            else {
+                setMessage(`You've successfully logged in!`);
+                setToken(result);
+            }
+        }
+        catch (err) {
+            setMessage("We couldn't reach our servers, make sure you are connected to internet and try again.");
         }
     }
 
     return (
         <div className="login-container">
             <div className="login">
-                <h1>{ message }</h1>
+                <h3 className="message">{ message }</h3>
 
-                <h4>Username:</h4>
-                <input placeholder="Username" onChange={ e => setUsername(e.target.value) } />
+                <h4>Username: </h4>
+                <input placeholder="AlbertEinstein69" onChange={ e => setUsername(e.target.value) } />
 
-                <h4>Password:</h4>
-                <input placeholder="Password" onChange={ e => setPassword(e.target.value) } />
+                <h4>Password: </h4>
+                {/* TODO: add a password show button */}
+                <input placeholder="password123" type="password" onChange={ e => setPassword(e.target.value) } />
 
                 <button onClick={ login }>
                     <p>Log In</p>
