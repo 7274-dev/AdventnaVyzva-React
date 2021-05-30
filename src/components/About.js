34b92126f5bd52ea7c4e17b2432ae7a8990da994
Daniel Svitan <dankosaurus9@gmail.com>
2021-05-30 13:53:24 +0200

    :sparkles: Adds Lorem Ipsum to about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,31 @@ import '../styles/About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="about-page">
+            <h2>Advent Challenge</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit lacus, viverra ullamcorper dis duis ac facilisi
+                habitasse, sem auctor habitant eros imperdiet netus cubilia. Cursus primis tempus felis mi eros duis
+                et a, magna libero urna ullamcorper donec mollis semper fermentum, commodo egestas vel tortor habitant
+                hac sit. Feugiat magnis nam venenatis condimentum euismod, ullamcorper nascetur ridiculus vivamus,
+                lobortis ultrices scelerisque ornare. Sociosqu duis tellus magnis suspendisse consequat sollicitudin
+                senectus fringilla dignissim penatibus, erat curae habitant neque sem sapien primis at iaculis, eget
+                ridiculus gravida metus per aptent potenti nullam inceptos. Penatibus aliquam arcu varius id neque
+                purus malesuada fringilla, montes in mollis pretium turpis sollicitudin tempus cum, libero hac magnis
+                ligula natoque magna sed. Aliquet facilisi ipsum adipiscing pellentesque sit porttitor placerat, eget
+                hendrerit lacinia rutrum ullamcorper elit sagittis scelerisque, libero donec tincidunt volutpat ligula
+                eu. Diam pellentesque nullam pharetra arcu suscipit luctus venenatis vestibulum, accumsan cubilia non
+                semper malesuada fusce mollis, praesent auctor nunc ultricies nulla metus ornare. Potenti cum consequat
+                gravida a lobortis viverra duis pretium placerat parturient porta, ultricies iaculis integer arcu
+                sociosqu cras dolor est massa ridiculus, cubilia senectus eget interdum nascetur condimentum mollis
+                lorem aliquet conubia. Amet vivamus torquent montes tristique conubia duis congue eu suscipit, curae
+                mollis senectus imperdiet morbi primis viverra accumsan, tempus molestie mi aliquet inceptos turpis at
+                scelerisque. Quisque lacus facilisis eu augue tristique scelerisque, nisl ad rutrum litora vehicula
+                curae interdum, ipsum accumsan tellus per leo. Metus hendrerit vivamus interdum neque rhoncus, erat
+                magnis sit imperdiet maecenas montes, aptent arcu duis fringilla. Hendrerit lectus lacinia ipsum neque
+                donec felis quis, eget auctor luctus nam sapien feugiat laoreet aenean, sociosqu natoque venenatis
+                ultrices pellentesque parturient.
+            </p>
         </div>
     )
 }
