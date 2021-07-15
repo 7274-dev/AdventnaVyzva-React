599e3bdef77bf53937ea3b33a84eb51e2e19e342
Daniel Svitan <dankosaurus9@gmail.com>
2021-07-15 09:53:39 +0200

    :sparkles: Adds password auth to admin page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,23 +1,58 @@
 import { useState } from 'react';
-import '../styles/Admin.css';
+import { sha512 } from 'js-sha512';
+import '../styles/Admin.css';  // do we really need Admin.css??
 
 const Admin = () => {
     // TODO code: finish this page (no need dark mode)
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [message, setMessage] = useState('Type your password:');
+    const [passwordInput, setPasswordInput] = useState(undefined);
+    const [showPassword, setShowPassword] = useState(false);
 
-    if (!isLoggedIn) {
-        return (
-            <div>
-                <h1>Please enter password: </h1>
-                { /* TODO code: password authentication */ }
-            </div>
-        )
+    const logIn = () => {
+        const enteredPassword = passwordInput.target.value;
+
+        if (sha512(enteredPassword) === 'fb0ed71ec21c9f4ac68add2abf631ad0e45c1f10ad35aac7bc44869cd40caa69bbf899800d62d6f98b2e2ff19163c446762e11960e1741ef49dbea3da5a72fcb') {
+            setIsLoggedIn(true);
+        }
+        else if (enteredPassword === 'your password') {
+            setMessage('Wrong password, try again:');
+        }
+        if (enteredPassword === 'again') {
+            setMessage('Sorry, your password is incorrect.');
+        }
+        else if (enteredPassword === 'incorrect') {
+            setMessage('Stop it before I call the cops.')
+        }
+        else {
+            setMessage('Wrong password, try again:');
+        }
+    }
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
     }
 
     return (
-        <div>
-            <h1>Admin btw</h1>
+        <div className='admin-page'>
+            { !isLoggedIn &&
+            <div className='login'>
+                <h1>{ message }</h1>
+
+                <input placeholder='UrMomPussy2Wet' type={ showPassword ? 'text' : 'password' } onChange={ setPasswordInput } />
+                <br />
+                <div className='show-password-container'>
+                    <input type='checkbox' onClick={ togglePasswordVisibility } />
+                    <p>Show Password</p>
+                </div>
+
+                <button type='submit' onClick={ logIn }>Ok</button>
+            </div> }
+
+            { isLoggedIn &&
+            <div className='content'>
+            </div> }
         </div>
     )
 }
