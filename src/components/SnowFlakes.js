3aa8adb28bc7136bae2ffcef2c00a3243333da9b
Daniel Svitan <dankosaurus9@gmail.com>
2021-08-03 12:22:18 +0200

    :art: Changes snowflakes

--- src/components/SnowFlakes.js
+++ src/components/SnowFlakes.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useResponsiveValue } from '../hooks/useResponsiveValue';
 import { Snowfall, Snowflake } from 'react-snowflakes';
-import SnowflakeImage from '../images/snowflake.png';
+import SnowflakeImage1 from '../images/snowflake1.png';
+import SnowflakeImage2 from '../images/snowflake2.png';
+import SnowflakeImage3 from '../images/snowflake3.png';
 import '../styles/SnowFlakes.css';
 
 const SnowFlakes = ({ snowflakes }) => {
     const [currentOpacity, setCurrentOpacity] = useState(snowflakes ? 1 : 0);
     const [currentIntervalID, setCurrentIntervalID] = useState(undefined);
-    const snowFlakeCount = useResponsiveValue(200, 20);
+    const snowFlakeCount = useResponsiveValue(150, 20);
 
     // snowflake opacity animation
     useEffect(() => {
@@ -35,7 +37,16 @@ const SnowFlakes = ({ snowflakes }) => {
 
         id = setInterval(frame, 50);
         setCurrentIntervalID(id);
+    // dont add any more dependencies or it will not work (by Streamer272)
     }, [snowflakes]);
+
+    const getRandomSnowflakeImage = () => {
+        const rand = Math.floor(Math.random() * 3 + 1);
+        if (rand === 1) return SnowflakeImage1;
+        if (rand === 2) return SnowflakeImage2;
+        if (rand === 3) return SnowflakeImage3;
+        return null;
+    }
 
     return (
         <div className='snowflakes' style={{opacity: currentOpacity}}>
@@ -46,10 +57,10 @@ const SnowFlakes = ({ snowflakes }) => {
                     height: '100vh'
                 }}
                 snowflakeFactory={ index => {
-                    // TODO graphics: change snowflake image
+                    // TODO graphics: change snowflake colors
                     return (
                         <Snowflake speed={ 2.5 } xSpeedPrc={ .3 } ySpeedPrc={ .1 } className='snowflake'>
-                            <img alt='Snowflake' src={ SnowflakeImage } className='unselectable' />
+                            <img alt='Snowflake' src={ getRandomSnowflakeImage() } className='unselectable' />
                         </Snowflake>
                     )
                 } } />
