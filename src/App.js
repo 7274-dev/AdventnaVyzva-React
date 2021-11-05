f9505f5482f15174769ed3834234a5750dbffa59
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-11-05 21:53:54 +0100

    :hammer: Refactors Yarn readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-// TODO all: translate everything to SLOVAK language
 // TODO management: generate MIT license here: https://license-generator.intm.org/
 // TODO code: add some easter eggs
 // TODO code: check if there are no semicolons missing
 // TODO code: fix performance
 // TODO code, design: focus on student responsibility
 // TODO management: write README
-// Q: do we want support/feedback site
-// A: no
-// Q: what languages?
-// A: en and sk
 
 import { useState, useEffect } from 'react';
 import { useDefaultValue } from './hooks/useDefaultValue';
