0d09f92c1122121c1e2426ddd309c8c15d1a1fb8
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-11-13 15:05:15 +0100

    :zap: Adds transparent background to Colors.scss

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,4 +1,3 @@
-import { useTheme } from '../../App';
 import { localized } from '../../hooks/useLocalization';
 import LoadingVideo from '../../images/loading.mp4';
 import './Loading.css';
@@ -6,10 +5,8 @@ import './Loading.css';
 const Loading = () => {
     // Q: do we want loading? yes
 
-    const loadingComponentClassName = useTheme('loading-component');
-
     return (
-        <div className={ loadingComponentClassName }>
+        <div className='loading'>
             <h1>{ localized('loading.title') }</h1>
 
             <video src={ LoadingVideo } controls={ false } autoPlay loop />
