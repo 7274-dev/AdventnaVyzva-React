8fa57b22ec9c98ad4a6c90e28a50177785ef5efc
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-11-20 16:31:18 +0100

    :bug: Fixes submission

--- src/pages/TeacherPage/HomeworkSection/Submission.js
+++ src/pages/TeacherPage/HomeworkSection/Submission.js
@@ -1,17 +1,18 @@
-import { Attachment } from '../../../components';
+import { Attachment, redirectMeTo } from '../../../components';
 import { isDefined } from '../../../hooks/isDefined';
 import { localized } from '../../../hooks/useLocalization';
 import './HomeworkSection.css';
 
 const Submission = ({ data }) => {
-    console.log(`submission`, data)
-
     if (!isDefined(data)) {
         return null;
     }
+    // noinspection JSUnresolvedVariable
     return (
         <div className='submission'>
+            <h1>{ data.id }, { localized('teacherPage.submission.by') } <a onClick={() => redirectMeTo(`/teacher/student/${data.user.userId}`)}>{ data.user.userName }</a></h1>
             <h1>{ localized('teacherPage.submission.content') }: { data.content }</h1>
+
             { data?.fileIds?.map((fileId, index) => <Attachment key={ index } data={ fileId } />) }
         </div>
     )
