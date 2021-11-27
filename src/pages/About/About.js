71952c37ed363fb48ed521c73c7807b3510c2357
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-11-27 14:08:16 +0100

    :hammer: Refactors about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -22,20 +22,29 @@ const About = () => {
                     plniť úlohy. Otestujú si nielen svoje vedomosti a logické myslenie, ale aj schopnosť  tvorenia si vlastného time-managementu.  </li>
                 <li><strong>Dátum spustenia: </strong>1. December 2021</li>
 
-                <li> <strong>Manažéri projektu: </strong></li>
-                <ol>
-                    <li>Veronika Kubicová</li>
-                <ul><strong>Kontakt: </strong>veronikakubicova.80@gmail.com</ul>
+                <li><strong>Manažéri projektu: </strong></li>
+                <ol style={{padding: '0 0 0 1.5rem'}}>
+                    <li>
+                        <ul style={{padding: '0'}}>
+                            <li>Veronika Kubicová</li>
+                            <li><strong>Kontakt: </strong>veronikakubicova.80@gmail.com</li>
+                        </ul>
+                    </li>
 
-                <li>Slávka Košecká </li>
-                <ul><strong>Kontakt: </strong>slavka.kosecka@gmail.com</ul> </ol><br />
+                    <li>
+                        <ul style={{padding: '0'}}>
+                            <li>Slávka Košecká </li>
+                            <li><strong>Kontakt: </strong>slavka.kosecka@gmail.com</li>
+                        </ul>
+                    </li>
+                </ol><br />
 
                 <li><strong>Tvorca: </strong>Andrea Draganová<br /></li>
                 <ul>
                     <li>
-                        Pani učiteľka pochádzajca z Oravy ale momentálne pôsobý na škole v Petržalke.
+                        Pani učiteľka pochádza z Oravy ale momentálne pôsobý na škole v Petržalke.
                         Jej ambíciou bolo vytvoriť stránku, ktorá užívateľov nielen zaujme, ale ich
-                        aj výborne pripraví na blížiace sa Vianoce.
+                        aj skvelo uvedie do Vianočného obdobia.
                     </li>
                     <li><strong>Kontakt: </strong>andrea.draganova@svr.sk</li>
                 </ul>
@@ -45,36 +54,50 @@ const About = () => {
 
                 <li><strong>Developeri: </strong><br /></li>
                 <ol>
-                    <strong><li>Daniel Svitaň</li></strong>
+                    <li><strong>Daniel Svitaň</strong></li>
                     <ul>
-                        <li>Viedol a poháňal celý tím. Svoje úlohy plnil precízne a vždy načas.
-                            Jeho silnou stránkou je predovšetkým vytrvalosť a odhodlanosť.
-                            Málokto by venoval tvorbe stránky tak veľa času ako on.
+                        <li>
+                            Viedol a poháňal celý tím. Jeho silnou stránkou je predovšetkým vytrvalosť
+                            a odhodlanosť. Málokto by venoval tvorbe stránky tak veľa času ako on.
                         </li>
                         <li><strong>Kontakt: </strong>daniel.svitan.team7274dev@gmail.com</li>
                     </ul>
 
-                    <strong><li>Jakub Tulek</li></strong>
-                    Je skvelý tímový hráč a rýchlo sa učí nové veci. Je jazykovo zdatný,
-                    a tak sa venoval aj prekladu stránky. Prioritne sa zaslúžil
-                    o tvorbu samotného srdca celej stránky – servera.
-                    <ul><strong>Kontakt: </strong>jtulek@at-rt.com</ul><br />
+                    <li><strong>Jakub Tulek</strong></li>
+                    <ul>
+                        <li>
+                            Je skvelý tímový hráč a rýchlo sa učí nové veci. Je jazykovo zdatný,
+                            a tak sa venoval aj prekladu stránky. Prioritne sa zaslúžil
+                            o tvorbu samotného srdca celej stránky – servera.
+                        </li>
+                        <li><strong>Kontakt: </strong>jtulek@at-rt.com</li><br />
+                    </ul>
 
-                    <strong><li>Jakub Porubec</li></strong>
-                    Je veľmi tvorivý a má dobrú predstavivosť.
-                    Rád tvorí indie hry a tvorbu grafiky má v malíčku.
-                    Venuje sa najmä pixel-art a 3D grafike.
-                    <ul><strong>Kontakt: </strong>zmakamk@gmail.com</ul><br />
+                    <li><strong>Jakub Porubec</strong></li>
+                    <ul>
+                        <li>
+                            Je veľmi tvorivý a má dobrú predstavivosť.
+                            Rád tvorí indie hry a tvorbu grafiky má v malíčku.
+                            Venuje sa najmä pixel-art a 3D grafike.
+                        </li>
+                        <li><strong>Kontakt: </strong>zmakamk@gmail.com</li><br />
+                    </ul>
 
-                    <strong><li>Richard Sepši</li></strong>
-                    V počítačovom svete a vo svete hier je ako doma.
-                    Tiež ho bavia cudzie jazyky a rád sa ich učí.
-                    Takisto je aj zručný grafik s estetickým
-                    ako aj počítačový hráč, a tak rozhodoval predovšetkým o vzhľade stránky.
-                    <ul><strong>Kontakt: </strong>rihvis@gmail.com</ul><br />
+                    <li><strong>Richard Sepši</strong></li>
+                    <ul>
+                        <li>
+                            V počítačovom svete a vo svete hier je ako doma.
+                            Tiež ho bavia cudzie jazyky a rád sa ich učí.
+                            Takisto je aj zručný grafik s estetickým
+                            ako aj počítačový hráč, a tak rozhodoval predovšetkým o vzhľade stránky.
+                        </li>
+                        <li><strong>Kontakt: </strong>rihvis@gmail.com<br /></li>
+                    </ul>
 
-                    <strong><li>Ivan Hrabčák</li></strong>
-                    <ul><strong>Kontakt: </strong>ivan@hrabcak.eu</ul><br />
+                    <li><strong>Ivan Hrabčák</strong></li>
+                    <ul>
+                        <li><strong>Kontakt: </strong>ivan@hrabcak.eu</li><br />
+                    </ul>
                 </ol>
 
                 <li><strong>Mesto, krajina: </strong>Bratislava, Slovensko</li>
